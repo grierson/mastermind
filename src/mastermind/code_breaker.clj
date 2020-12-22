f94435e6d67968b72ed5ca11ac192c37f8b670cf
Kyle Grierson <griersonky@gmail.com>
2020-12-22 20:28:45 +0000

    code maker

--- src/mastermind/code_breaker.clj
+++ src/mastermind/code_breaker.clj
@@ -1,5 +1,23 @@
 (ns mastermind.code-breaker)
 
-(defn break-code [gussess]
-  [0 0 0 0])
+(defn guess->number [guess]
+  (reduce #(+ (* 6 %1) %2) guess))
+
+(defn number->guess [number]
+  [(rem (quot number 216) 6)
+   (rem (quot number 36) 6)
+   (rem (quot number 6) 6)
+   (rem number 6)])
+
+(defn inc-guess [guess]
+  (-> guess
+      guess->number
+      inc
+      number->guess))
+
+
+(defn break-code [last-guess past-guesses]
+  (if (nil? last-guess)
+    [0 0 0 0]
+    [1 1 1 1]))
 
--- test/mastermind/code_breaker_test.clj
+++ test/mastermind/code_breaker_test.clj
@@ -2,7 +2,26 @@
   (:require [midje.sweet :refer :all]
             [mastermind.code-breaker :refer :all]))
 
+
+
 (facts "Code Breaker"
+  (fact "guess-to-number"
+    (guess->number [0 0 0 0]) => 0
+    (guess->number [0 0 0 1]) => 1
+    (guess->number [5 5 5 5]) => (dec (* 6 6 6 6)))
+
+  (fact "number to guess"
+    (number->guess 0) => [0 0 0 0]
+    (number->guess (dec (* 6 6 6 6))) => [5 5 5 5])
+
+  (fact "increment guess"
+    (inc-guess [0 0 0 0]) => [0 0 0 1]
+    (inc-guess [5 5 5 5]) => [0 0 0 0])
+
   (fact "initial guess"
-    (break-code []) => [0 0 0 0]))
+    (break-code nil []) => [0 0 0 0])
+
+  (fact "first step for [1 2 3 4]"
+    (break-code [0 0 0 0]
+                [[[0 0 0 0] [0 0]]]) => [1 1 1 1]))
 
